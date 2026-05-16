bc5ecbe9c8bd84b3c0e02b9ee7034b7f4ec1142c
tanmayrane18 <tanmayrane51@gmail.com>
2026-05-16 12:45:11 +0530

    expo clean up done

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import React from "react";
+import { useColorScheme } from "react-native";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { Stack } from "expo-router";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
